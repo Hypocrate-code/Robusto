fcecc10a548f3cddedae586fd0e49e6a01960d70
Hypocrate-code <112576942+Hypocrate-code@users.noreply.github.com>
2022-12-04 23:40:28 +0100

    Add files via upload

---
+++ thibaut-alvoet_Leaf.js
@@ -0,0 +1,26 @@
+const coffeeJar = document.querySelector(".coffee-pot-icon");
+const coffeeJarContainer = document.querySelector(".coffee-pot-container");
+const coffeeCup = document.querySelector(".coffee-cup");
+let after_of_coffeeJarContainer = window.getComputedStyle(coffeeJarContainer, '::after');
+var coffeeDown = document.getElementById('coffee-down');
+var coffeeUp = document.getElementById('coffee-up');
+
+coffeeJar.addEventListener('click', function() {
+
+    if (after_of_coffeeJarContainer.width=='0px'){
+        coffeeJar.classList.add("active");
+        coffeeCup.classList.add('out');
+        coffeeJarContainer.style.setProperty('--animation', 'liquid')
+        coffeeDown.beginElement();
+        coffeeJarContainer.setAttribute("aria-expanded", true);
+    }
+    else {
+        coffeeJar.classList.remove("active");
+        coffeeCup.classList.remove('out');
+        coffeeJarContainer.style.setProperty('--animation', 'liquid-reverse');
+        coffeeUp.beginElement();
+        coffeeJarContainer.setAttribute("aria-expanded", false);
+    }
+});
+
+
